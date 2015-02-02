7ce0f6e78151120edf0a204499f9a2302f751b96
botechnic <maxuejun@botechnic.com>
2015-02-02 11:14:24 +0800

    add node_modules

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 81;
 
 var mj_playback = require('./server/mj_playback');
 var mj_playlive = require('./server/mj_playlive');
